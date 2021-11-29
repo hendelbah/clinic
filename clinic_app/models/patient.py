fd72764b2f03f8034333876bc5ba8c98d98d81fc
hendelbah <vowava.567@gmail.com>
2021-11-29 22:34:40 +0200

    improved population

--- clinic_app/models/patient.py
+++ clinic_app/models/patient.py
@@ -13,28 +13,25 @@ class Patient(BaseModel):
     __tablename__ = 'patient'
 
     phone_number = db.Column(db.String(20), nullable=False, unique=True, index=True)
-    email = db.Column(db.String(80), unique=True, index=True)
-    first_name = db.Column(db.String(40), nullable=False)
-    last_name = db.Column(db.String(40), nullable=False, index=True)
-    third_name = db.Column(db.String(40), nullable=False)
+    surname = db.Column(db.String(40), nullable=False, index=True)
+    name = db.Column(db.String(40), nullable=False)
+    patronymic = db.Column(db.String(40), nullable=False)
     birthday = db.Column(db.Date, nullable=False)
     time_registered = db.Column(db.DateTime, server_default=db.func.now())
 
     booked_appointments = db.relationship('BookedAppointment', back_populates='patient')
     fulfilled_appointments = db.relationship('FulfilledAppointment', back_populates='patient')
 
-    def __init__(self, phone_number, first_name, last_name, third_name, birthday, email=None):
+    def __init__(self, phone_number, surname, name, patronymic, birthday):
         """
         :param str phone_number: patient's unique phone number
-        :param str first_name: patient's given name
-        :param str last_name: patient's surname
-        :param str third_name: patient's third name
+        :param str surname: patient's surname
+        :param str name: patient's given name
+        :param str patronymic: patient's patronymic
         :param date birthday: patient's date of birth
-        :param str email: patient's email
         """
         self.phone_number = phone_number
-        self.first_name = first_name
-        self.last_name = last_name
-        self.third_name = third_name
+        self.surname = surname
+        self.name = name
+        self.patronymic = patronymic
         self.birthday = birthday
-        self.email = email
--- clinic_app/service/db_population.py
+++ clinic_app/service/db_population.py
@@ -1,30 +1,51 @@
-from random import choices
-from string import digits
+"""
+This module stands for populating database
+"""
 from datetime import date
+from random import choice, randrange
 from werkzeug.security import generate_password_hash
-from clinic_app.models import *
-from .population_data import *
+from clinic_app.service.utils import random_9d_number, random_date
+from clinic_app.models import MedicalArea, Doctor, User, Patient, BookedAppointment, FulfilledAppointment, db
+from clinic_app.service.population_data import AREAS_SRC, DOCTORS_SRC, NAMES_SRC, SURNAMES_SRC, PATRONYMICS_SRC
+
+ADMIN_PASSWORD = 'yBU6nWPoVRLTtUl'
+DOCTORS_PASSWORD = 'cepjaKotDsM5PnG'
 
 
 def populate():
     """
-    Clear tables and populate database with sample data, using population_data source.
+    Clear tables and populate database with sample data, using population_data.
     """
-    # User.query.filter(User.email.like('doctor\___@spam.ua')).delete(synchronize_session=False)
-    BookedAppointment.query.delete()
-    FulfilledAppointment.query.delete()
+    # BookedAppointment.query.delete()
+    # FulfilledAppointment.query.delete()
     Patient.query.delete()
-    User.query.delete()
-    Doctor.query.delete()
-    MedicalArea.query.delete()
+    # User.query.delete()
+    # Doctor.query.delete()
+    # MedicalArea.query.delete()
+    #
+    # root_user = User('clinic_admin', ADMIN_PASSWORD, is_admin=True)
+    # db.session.add(root_user)
+    # medical_areas = [MedicalArea(*area_data) for area_data in AREAS_SRC]
+    # # hashing is too slow to do it for every user
+    # doctors_pass_hash = generate_password_hash(DOCTORS_PASSWORD)
+    # for i, doctor_src in enumerate(DOCTORS_SRC):
+    #     doctor = Doctor(*doctor_src['data'])
+    #     doctor.medical_area = medical_areas[doctor_src['area_index']]
+    #     user_doctor = User(f'doctor_{i:0>2}@spam.ua', doctors_pass_hash, _hash=False)
+    #     user_doctor.doctor = doctor
+    #     db.session.add(user_doctor)
+    patients_src = []
+    for i in range(1, 201):
+        sex = randrange(2)
+        patient = {
+            'id': i,
+            'phone_number': '+380' + random_9d_number(),
+            'surname': choice(SURNAMES_SRC),
+            'name': choice(NAMES_SRC[sex]),
+            'patronymic': choice(PATRONYMICS_SRC[sex]),
+            'birthday': random_date(1950, 2005)
+        }
+        patients_src.append(patient)
 
-    medical_areas = [MedicalArea(*area_data) for area_data in AREAS_SRC]
-    doctors_pass_hash = generate_password_hash('Doctor1488_322')
-    for i, doctor_src in enumerate(DOCTORS_SRC):
-        doctor = Doctor(*doctor_src['data'])
-        doctor.medical_area = medical_areas[doctor_src['area_index']]
-        user_doctor = User(f'doctor_{i:0>2}@spam.ua', doctors_pass_hash, _hash=False)
-        print(user_doctor)
-        user_doctor.doctor = doctor
-        db.session.add(user_doctor)
-    db.session.commit()
+    db.session.execute(db.insert(Patient).values(patients_src))
+    # db.session.commit()

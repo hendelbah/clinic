8ebc79a88976caf1c72a40e9840941a392b26862
hendelbah <vowava.567@gmail.com>
2021-12-03 04:54:00 +0200

    added service

--- clinic_app/service/__init__.py
+++ clinic_app/service/__init__.py
@@ -1,2 +1,12 @@
+"""
+Package contain service classes and functions to work with database
+"""
+from clinic_app.service.basic_query_helper import BasicQueryHelper
+from clinic_app.service.population import populate
 
+def func():
+    r = BasicQueryHelper.exists(None)
 
+    print(r)
+
+    x = {}

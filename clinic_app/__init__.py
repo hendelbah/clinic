993c369621f0c237f0971112a54d823dc6c5bdc8
hendelbah <vowava.567@gmail.com>
2021-12-04 23:24:49 +0200

    restructured app initialization

--- clinic_app/__init__.py
+++ clinic_app/__init__.py
@@ -1,61 +1,50 @@
 """
 Package stores all source files of project.
-This module provides create_app function for initializing Flask-Application
+This module stands for app initialization and configuring
 """
+import sys
 import logging
 from logging.handlers import RotatingFileHandler
-from flask import Flask, has_request_context, request
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
-from clinic_app.config import app_config
+from clinic_app.config import CurrentConfig, BASE_DIR
 
+app = Flask(__name__)
+app.config.from_object(CurrentConfig)
+print(CurrentConfig.SQLALCHEMY_DATABASE_URI)
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
-class RequestFormatter(logging.Formatter):
-    def format(self, record):
-        if has_request_context():
-            record.url = request.url
-            record.remote_addr = request.remote_addr
-        else:
-            record.url = None
-            record.remote_addr = None
+migration = Migrate(app, db)
 
-        return super().format(record)
+login_manager = LoginManager(app)
+login_manager.login_message = "You must be logged in to access this page."
+login_manager.login_view = "auth.login"
 
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s: %(message)s')
+log_path = BASE_DIR / 'logs' / 'clinic.log'
+file_handler = RotatingFileHandler(log_path, maxBytes=10240, backupCount=10)
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.DEBUG)
 
-db = SQLAlchemy()
-login_manager = LoginManager()
-ma = Marshmallow()
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setFormatter(formatter)
+console_handler.setLevel(logging.DEBUG)
 
+app.logger.handlers.clear()
+app.logger.addHandler(file_handler)
+app.logger.addHandler(console_handler)
+app.logger.setLevel(logging.DEBUG)
+app.logger.info('App initialization')
+werkzeug_logger = logging.getLogger('werkzeug')
+werkzeug_logger.handlers.clear()
+werkzeug_logger.addHandler(file_handler)
+werkzeug_logger.addHandler(console_handler)
+werkzeug_logger.setLevel(logging.DEBUG)
 
-def create_app(config_name):
-    """Create and configure an instance of the Flask application."""
-    app = Flask(__name__)
-    conf = app_config[config_name]
-    app.config.from_object(conf)
-    # import clinic_app.models
-    db.app = app
-    db.init_app(app)
-    ma.init_app(app)
-    Migrate(app, db)
-    login_manager.init_app(app)
-    login_manager.login_message = "You must be logged in to access this page."
-    login_manager.login_view = "auth.login"
-    request_formatter = RequestFormatter(
-        '[%(asctime)s] %(remote_addr)s requested %(url)s\n'
-        '%(levelname)s in %(module)s: %(message)s'
-    )
-    file_handler = RotatingFileHandler(
-        conf.BASE_DIR / 'logs' / 'clinic.log', maxBytes=10240, backupCount=10
-    )
-    file_handler.setFormatter(request_formatter)
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-    app.logger.setLevel(logging.INFO)
-    app.logger.info('App startup')
+from clinic_app.rest import api_blueprint
 
-    from rest import api_blueprint
-    app.register_blueprint(api_blueprint)
-
-    return app
+app.register_blueprint(api_blueprint)
--- clinic_app/config.py
+++ clinic_app/config.py
@@ -4,26 +4,28 @@ This module contains all configuration data for web application
 import pathlib
 import os
 
-
+BASE_DIR = pathlib.Path(__file__).parent
+FLASK_CONFIG = os.getenv('FLASK_CONFIG')
 DB_USER = os.getenv('FLASK_DB_USER')
 DB_PASS = os.getenv('FLASK_DB_PASSWORD')
+DB_SERVER = os.environ.get('FLASK_DB_SERVER')
 DB_NAME = os.getenv('FLASK_DB_NAME')
+FLASK_SECRET_KEY = os.getenv('FLASK_SECRET_KEY')
 
 
 class Config(object):
     """
     Common configurations
     """
-    SECRET_KEY = os.getenv('FLASK_SECRET_KEY')
+    SECRET_KEY = FLASK_SECRET_KEY
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    BASE_DIR = pathlib.Path(__file__).parent
 
 
 class DevelopmentConfig(Config):
     """
     Development configurations
     """
-    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{DB_USER}:{DB_PASS}@localhost/clinic_test'
+    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_SERVER}/clinic_test'
     TESTING = True
     SQLALCHEMY_ECHO = True
     DEBUG = True
@@ -33,11 +35,13 @@ class ProductionConfig(Config):
     """
     Production configurations
     """
-    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{DB_USER}:{DB_PASS}@localhost/{DB_NAME}'
+    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_SERVER}/{DB_NAME}'
     DEBUG = False
 
 
-app_config = {
+config_name = {
     'development': DevelopmentConfig,
     'production': ProductionConfig
 }
+
+CurrentConfig = config_name[FLASK_CONFIG]

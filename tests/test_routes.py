1212bc5a588063ee57d76c635fc623742fef7a2d
hendelbah <vowava.567@gmail.com>
2021-12-13 22:46:48 +0200

    fixed tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -14,14 +14,13 @@ class TestRoutes(BaseTestCase):
                 self.assert200(response)
 
     def test_login_logout(self):
-        with self.client as client:
-            response = client.post(url_for('auth.login'),
-                                   json={'email': 'root', 'pwd': ROOT_PASSWORD, 'remember': False})
-            self.assertStatus(response, 302)
-            response = self.client.get(url_for('auth.profile'))
-            self.assertStatus(response, 200)
-            response = self.client.get(url_for('auth.logout'))
-            self.assertStatus(response, 302)
+        response = self.client.post(url_for('auth.login'),
+                                    json={'email': 'root', 'pwd': ROOT_PASSWORD, 'remember': False})
+        self.assertStatus(response, 302)
+        response = self.client.get(url_for('auth.profile'))
+        self.assertStatus(response, 200)
+        response = self.client.get(url_for('auth.logout'))
+        self.assertStatus(response, 302)
 
     def test_wrong_login_logout(self):
         with self.client as client:

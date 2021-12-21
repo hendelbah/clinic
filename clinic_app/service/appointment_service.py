7906beeb1748d2dda1c055be00fd2ef9e8e7b120
hendelbah <vowava.567@gmail.com>
2021-12-22 01:10:04 +0200

    backup appointment_service.py

--- clinic_app/service/appointment_service.py
+++ clinic_app/service/appointment_service.py
@@ -17,29 +17,38 @@ class AppointmentService(BaseService):
 
     @classmethod
     def _filter_by(cls, *, doctor_uuid: str = None, patient_uuid: str = None,
+                   doctor_name: str = None, patient_name: str = None,
                    date_from: date_ = None, date_to: date_ = None,
                    unfilled: bool = False, _query=None) -> Query:
         """
         Return query ordered and filtered.
 
-        :param doctor_uuid: filter booked appointments related to doctors with this uuid
-        :param patient_uuid: filter booked appointments related to patients with this uuid
-        :param date_from: filter served appointments having date >= given value
-        :param date_to: filter served appointments having date <= given value
-        :param unfilled: filter booked appointments before now
+        :param doctor_uuid: filter appointments related to doctor with this uuid
+        :param patient_uuid: filter appointments related to patients with this uuid
+        :param doctor_name: filter appointments related to doctor with full name like this
+        :param patient_name: filter appointments related to patient with full name like this
+        :param date_from: filter appointments having date >= given value
+        :param date_to: filter appointments having date <= given value
+        :param unfilled: filter appointments with no bill
         :param _query: query to override standard one
         """
         query = cls._order() if _query is None else _query
         if doctor_uuid is not None:
             query = query.filter_by(
                 doctor_id=
-                cls.db.session.query(Doctor.id).filter_by(uuid=doctor_uuid).scalar_subquery()
-            )
+                cls.db.session.query(Doctor.id).filter_by(uuid=doctor_uuid).scalar_subquery())
         if patient_uuid is not None:
             query = query.filter_by(
                 patient_id=
-                cls.db.session.query(Patient.id).filter_by(uuid=patient_uuid).scalar_subquery()
-            )
+                cls.db.session.query(Patient.id).filter_by(uuid=patient_uuid).scalar_subquery())
+        if doctor_name is not None:
+            query = query.filter(Doctor.query
+                                 .filter(Doctor.full_name.like(f'%{doctor_name}%'))
+                                 .filter_by(id=cls.model.doctor_id).exists())
+        if patient_name is not None:
+            query = query.filter(Patient.query
+                                 .filter(Patient.full_name.like(f'%{patient_name}%'))
+                                 .filter_by(id=cls.model.patient_id).exists())
         if date_from == date_to is not None:
             query = query.filter_by(date=date_from)
         else:
@@ -70,4 +79,4 @@ class AppointmentService(BaseService):
         :param filters: kwargs for filtering function
         """
         query = cls.db.session.query(cls.db.func.sum(cls.model.bill))
-        return int(cls._filter_by(_query=query, **filters).scalar())
+        return int(cls._filter_by(_query=query, **filters).scalar() or 0)
